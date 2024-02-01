3c9eea286d1a3facdd74482e7db3848844d03462
User <Willspri@gmail.com>
2024-02-01 16:34:34 +1300

    instructions and plan

--- EnigmaCrib/EnigmaCrib.py
+++ EnigmaCrib/EnigmaCrib.py
@@ -4,3 +4,9 @@ plaintext  = "SECRETMESSAGE"
 #Complete the code here to output all possible valid cribs using the ciphertext and the plaintext to use for the crib
 #You will need to test all posible positions of the crib and discard cribs containing at least one letter encoded as itself.
 
+#iterate through each index of each string if indexes contain the same letter
+#shift character in position 0 of plaintext by one padding 
+#plaintext with 0 in front
+#print results that do not contain matches 
+    
+    
