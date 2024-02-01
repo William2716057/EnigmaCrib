63ea526235531f5cdc13dab917e4b35dcdcd532a
User <Willspri@gmail.com>
2024-02-01 16:23:53 +1300

    Add project files.

---
+++ EnigmaCrib/EnigmaCrib.py
@@ -0,0 +1,6 @@
+ciphertext = "HQEYSAWQSTNTLGKPESREVL"
+plaintext  = "SECRETMESSAGE"
+
+#Complete the code here to output all possible valid cribs using the ciphertext and the plaintext to use for the crib
+#You will need to test all posible positions of the crib and discard cribs containing at least one letter encoded as itself.
+
