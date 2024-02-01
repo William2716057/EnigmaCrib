71196207f36ff9e29023b47276ad215dfda8b4a1
User <Willspri@gmail.com>
2024-02-01 16:58:55 +1300

    Finds first iteration that is not valid

--- EnigmaCrib/EnigmaCrib.py
+++ EnigmaCrib/EnigmaCrib.py
@@ -8,5 +8,12 @@ plaintext  = "SECRETMESSAGE"
 #shift character in position 0 of plaintext by one padding 
 #plaintext with 0 in front
 #print results that do not contain matches 
-    
-    
+
+#add cipher and plaintext to lists 
+#if not print result
+#"H","Q","E","Y","S","A","W","Q","S","T","N","T","L","G","K","P","E","S","R","E","V","L"
+list1, list2 = ["H","Q","E","Y","S","A","W","Q","S","T","N","T","L","G","K","P","E","S","R","E","V","L"], ["S","E","C","R","E","T","M","E","S","S","A","G","E"]
+
+print([index for index, (e1, e2) in enumerate(zip(list1, list2)) if e1 == e2])
+#match at [8]
+#list2 = ["0","S","E","C","R","E","T","M","E","S","S","A","G","E"]
